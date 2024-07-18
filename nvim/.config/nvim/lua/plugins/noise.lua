c9126fe71f6129b5a6134f942c5a87eb01540ae0
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-07-18 11:44:29 +0300

    Remove nvim-notify

--- nvim/.config/nvim/lua/plugins/noise.lua
+++ nvim/.config/nvim/lua/plugins/noise.lua
@@ -10,7 +10,7 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 	config = function()
 		require("noice").setup({
@@ -31,8 +31,8 @@ return {
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 		})
-		require("notify").setup({
-			background_colour = "#000000"
-		})
+		-- require("notify").setup({
+		-- 	background_colour = "#000000"
+		-- })
 	end,
 }
