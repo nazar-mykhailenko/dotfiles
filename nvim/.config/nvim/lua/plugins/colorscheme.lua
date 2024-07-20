566a7cead75f7aeb8e6531188fe654a3e66d4ed0
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-07-20 10:52:22 +0300

    Change nvim colorscheme and terminal font

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
-	"rebelot/kanagawa.nvim",
-	config = function ()
-		require("kanagawa").setup({
-			transparent = true,
-		})
-		require("kanagawa").load("wave")
-	end
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require 'nordic' .load()
+    end
 }
+-- 	{
+-- 	"rebelot/kanagawa.nvim",
+-- 	config = function ()
+-- 		require("kanagawa").setup({
+-- 			transparent = false,
+-- 		})
+-- 		require("kanagawa").load("wave")
+-- 	end
+-- }
