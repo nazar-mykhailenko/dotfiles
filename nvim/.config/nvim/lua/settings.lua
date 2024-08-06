3441f1590a0e304c91bb2b81bb69598e43c327ad
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-08-06 17:10:55 +0300

    Add autocommand to delete whitespaces

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -19,3 +19,8 @@ opt.scrolloff = 10
 opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+local remove_whitespace_group = vim.api.nvim_create_augroup("remove_whitespace", { clear = true })vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+    group = remove_whitespace_group,
+})
