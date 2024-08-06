e0663ee3f54716ff31d9d5f30035559a8f026b41
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-08-06 17:12:25 +0300

    Made neotree close after opening file

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -5,10 +5,28 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		"3rd/image.nvim",            -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal right toggle<CR>",
-			{ desc = "Toggle [e]xplorer" })
+		require("neo-tree").setup({
+			event_handlers = {
+
+				{
+					event = "file_open_requested",
+					handler = function()
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+			window = {
+				mappings = {
+					["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal right toggle<CR>", { desc = "Toggle [e]xplorer" })
 	end,
 }
